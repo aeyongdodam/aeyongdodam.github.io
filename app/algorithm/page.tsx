e62643e46ea33d44f1a69414794f8b28243f3f33
aeyongdodam <kikititi0510@gmail.com>
2023-12-11 14:45:54 +0900

    feat: 메인 페이지 구성

---
+++ app/algorithm/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Algorithms from '@/components/algorithm/algorithm'
+
+export default function Algorithm() {
+  return <Algorithms />
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,31 @@
 import type { Config } from 'tailwindcss'
 
+function generateSpacing(): { [key: string]: string } {
+  const spacing: { [key: string]: string } = {
+    '30p': '30%',
+    '7p': '7%',
+    '9.2p': '9.2%',
+    '7.5p': '7.5%',
+    '8p': '8%',
+    '14p': '14%',
+    '24p': '24%',
+    '36p': '36%',
+    '38p': '38%',
+    '40p': '40%',
+    '45p': '45%',
+    '47p': '47%',
+    '80p': '80%',
+  }
+
+  for (let i = 28; i <= 900; i += 4) {
+    const key = `${i}p`
+    if (!spacing[key]) {
+      spacing[key] = `${i}%`
+    }
+  }
+  return spacing
+}
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,10 +34,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        '80p': '80%',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        '2p': '1.875rem',
+        '4p': '3.5rem',
+      },
+      spacing: generateSpacing(),
+      fill: {
+        current: 'currentColor',
+      },
+      fontFamily: {
+        abhaya: ['Abhaya Libre', 'serif'],
+      },
+      fontWeight: {
+        semibold: '600',
+        extrabold: '800',
       },
     },
   },

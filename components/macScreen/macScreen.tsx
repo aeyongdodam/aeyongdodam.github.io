9ab5f62899dcb22e2680a18e48f3a7e74309fe5e
aeyongdodam <kikititi0510@gmail.com>
2023-12-18 15:57:28 +0900

    fix: macScreen 컴포넌트 분리

--- components/macScreen/macScreen.tsx
+++ components/macScreen/macScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MacHeader from './macHeader'
 import MacContent from './macContent'
+import MacToggle from './MacToggle'
 
 const MacScreen = ({ text }: { text: string }) => {
   return (
     <div className="relative min-h-screen">
       <MacHeader text={text} />
+      <MacToggle />
       <MacContent />
     </div>
   )
